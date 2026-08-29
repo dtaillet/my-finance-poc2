b05df734ea6ec1a1df8c4462e66c35d926bdb496
TAILLET, Damien <tailletd@legal.regn.net>
2026-08-29 14:55:30 +0200

    pagination: add first last page

--- lib/ui/pagination.tsx
+++ lib/ui/pagination.tsx
@@ -17,6 +17,17 @@ export default function Pagination({ currentPage, totalPages }: { currentPage: n
     return (
         <nav className="flex items-center gap-x-1" aria-label="Pagination">
             {prevDisabled ? (
+                <span className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-muted-foreground-1 cursor-not-allowed" aria-label="First">
+                    <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m11 17-5-5 5-5" /><path d="m18 17-5-5 5-5" /></svg>
+                    <span className="sr-only">First</span>
+                </span>
+            ) : (
+                <Link href={createPageURL(1)} className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-foreground hover:bg-muted-hover focus:outline-hidden focus:bg-muted-focus disabled:opacity-50 disabled:pointer-events-none" aria-label="First">
+                    <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m11 17-5-5 5-5" /><path d="m18 17-5-5 5-5" /></svg>
+                    <span className="sr-only">First</span>
+                </Link>
+            )}
+            {prevDisabled ? (
                 <span className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-muted-foreground-1 cursor-not-allowed" aria-label="Previous">
                     <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6" /></svg>
                     <span className="sr-only">Previous</span>
@@ -41,6 +52,17 @@ export default function Pagination({ currentPage, totalPages }: { currentPage: n
                 <Link href={createPageURL(currentPage + 1)} className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-foreground hover:bg-muted-hover focus:outline-hidden focus:bg-muted-focus disabled:opacity-50 disabled:pointer-events-none" aria-label="Next">
                     <span className="sr-only">Next</span>
                     <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
+                </Link>
+            )}
+            {nextDisabled ? (
+                <span className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-muted-foreground-1 cursor-not-allowed" aria-label="Last">
+                    <span className="sr-only">Last</span>
+                    <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 17 5-5-5-5" /><path d="m13 17 5-5-5-5" /></svg>
+                </span>
+            ) : (
+                <Link href={createPageURL(totalPages)} className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-foreground hover:bg-muted-hover focus:outline-hidden focus:bg-muted-focus disabled:opacity-50 disabled:pointer-events-none" aria-label="Last">
+                    <span className="sr-only">Last</span>
+                    <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 17 5-5-5-5" /><path d="m13 17 5-5-5-5" /></svg>
                 </Link>
             )}
         </nav>
